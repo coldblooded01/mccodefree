488336f71a31f1ee51682ad8db748a7a28ae6117
Yeray Catalá <yeray.cv@protonmail.com>
2020-04-19 19:26:11 +0200

    Refactored index to use UserStats

--- upload/index.php
+++ upload/index.php
@@ -42,7 +42,7 @@ $h->userdata($user);
 $h->menuarea();
 
 print "<h3>General Info:</h2>";
-$exp = (int) ($user->exp / $user->exp_needed * 100);
+$exp = (int) ($user->exp / $user->get_exp_needed() * 100);
 print
         "<table><tr><td><b>Name:</b> {$user->username}</td><td><b>Crystals:</b> {$cm}</td></tr><tr>
 <td><b>Level:</b> {$user->level}</td>
@@ -53,19 +53,19 @@ print
 print "<hr><h3>Stats Info:</h3>";
 $ts = $user->user_stats->strength + $user->user_stats->agility
         + $user->user_stats->guard + $user->user_stats->labour
-        + $user->user_stats->iq;
+        + $user->user_stats->IQ;
 $formatted_stats = [];
 $formatted_stats['strank'] = get_rank($user->user_stats->strength, 'strength');
 $formatted_stats['agirank'] = get_rank($user->user_stats->agility, 'agility');
 $formatted_stats['guarank'] = get_rank($user->user_stats->guard, 'guard');
 $formatted_stats['labrank'] = get_rank($user->user_stats->labour, 'labour');
-$formatted_stats['IQrank'] = get_rank($user->user_stats->iq, 'IQ');
+$formatted_stats['IQrank'] = get_rank($user->user_stats->IQ, 'IQ');
 $tsrank = get_rank($ts, 'strength+agility+guard+labour+IQ');
 $formatted_stats['strength'] = number_format($user->user_stats->strength);
 $formatted_stats['agility'] = number_format($user->user_stats->agility);
 $formatted_stats['guard'] = number_format($user->user_stats->guard);
 $formatted_stats['labour'] = number_format($user->user_stats->labour);
-$formatted_stats['IQ'] = number_format($user->user_stats->iq);
+$formatted_stats['IQ'] = number_format($user->user_stats->IQ);
 $ts = number_format($ts);
 
 print
