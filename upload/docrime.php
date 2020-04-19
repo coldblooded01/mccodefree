fbaf4e2aeb29d2f4c7570b58b669137094b169a5
Yeray Catalá <yeray.cv@protonmail.com>
2020-04-19 19:48:14 +0200

    Refactored to use UserStats model

--- upload/docrime.php
+++ upload/docrime.php
@@ -56,7 +56,7 @@ if (!$_GET['c']) {
                 "\$sucrate="
                         . str_replace(array("LEVEL", "EXP", "WILL", "IQ"),
                                 array($user->level, $user->exp, $user->will,
-                                        $user->user_stats->iq), $r['crimePERCFORM']) . ";";
+                                        $user->user_stats->IQ), $r['crimePERCFORM']) . ";";
         eval($ec);
         print $r['crimeITEXT'];
         $ir['brave'] -= $r['crimeBRAVE'];
