cc7025d3d2ac1ce9e6f2937afade143edec5ea4f
Yeray Catalá <yeray.cv@protonmail.com>
2020-05-16 00:25:49 +0200

    Item model

--- upload/models/item.php
+++ upload/models/item.php
@@ -1,24 +1,35 @@
 <?php
 
 require_once(dirname(__FILE__) . "/../mysql.php");
+require_once(dirname(__FILE__) . "/item_type.php");
 
-class ItemType {
+class Item {
 
-    public function __construct($id, $name) {
+    public function __construct($id, $item_type, $name, $description, $buy_price, $sell_price, $buyable) {
         $this->id = $id;
+        $this->item_type = $item_type;
         $this->name = $name;
+        $this->description = $description;
+        $this->buy_price = $buy_price;
+        $this->sell_price = $sell_price;
+        $this->buyable = $buyable;
     }
 
     public static function create_from_mysqli_array($r) {
-        return new ItemType(
-            $r['itmtypeid'],
-            $r['itmtypename']
+        return new Item(
+            $r['itmid'],
+            ItemType::get($r['itmtype']),
+            $r['itmname'],
+            $r['itmdesc'],
+            $r['itmbuyprice'],
+            $r['itmsellprice'],
+            $r['itmbuyable']
         );
     }
 
-    public static function get_all($order_by='itmtypeid', $order_dir='ASC') {
+    public static function get_all($order_by='itmname', $order_dir='ASC') {
         global $c;
-        $query = "SELECT * FROM itemtypes ORDER BY {$order_by} {$order_dir}";
+        $query = "SELECT * FROM items ORDER BY {$order_by} {$order_dir}";
         $q = mysqli_query(
             $c,
             $query
@@ -34,7 +45,7 @@ class ItemType {
 
     public static function get($id) {
         global $c;
-        $query = "SELECT * FROM itemtypes WHERE itmtypeid={$id}";
+        $query = "SELECT * FROM items WHERE itmid={$id}";
         $q = mysqli_query(
             $c,
             $query
@@ -46,7 +57,7 @@ class ItemType {
 
     public static function exists($id) {
         global $c;
-        $query = "SELECT * FROM itemtypes WHERE itmtypeid={$id}";
+        $query = "SELECT * FROM items WHERE itmid={$id}";
         $q = mysqli_query(
             $c,
             $query
@@ -55,4 +66,9 @@ class ItemType {
         mysqli_free_result($q);
         return $num_rows != 0;
     }
+
+    public function is_buyable() {
+        return !!$this->buyable;
+    }
+
 }
